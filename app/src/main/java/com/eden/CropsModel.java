e7e23e7249d53d20c6d3f6e4c393ca30d28b0046
Adebayo2016 <adewaleadebayo630@gmail.com>
2021-02-06 16:09:31 +0100

    Initial Commit

---
+++ app/src/main/java/com/eden/CropsModel.java
@@ -0,0 +1,43 @@
+package com.eden;
+
+public class CropsModel {
+
+    private String CropName;
+    private String BotanicalName;
+    public Double shadeRequirement;
+
+    private CropsModel(){}
+
+
+    public Double getShadeRequirement() {
+        return shadeRequirement;
+    }
+
+    public void setShadeRequirement(Double shadeRequirement) {
+        this.shadeRequirement = shadeRequirement;
+    }
+
+    private CropsModel(String CropName, String BotanicalName, double shadeRequirement){
+
+        this.CropName=CropName;
+        this.BotanicalName=BotanicalName;
+        this.shadeRequirement=shadeRequirement;
+
+    }
+
+    public String getCropName() {
+        return CropName;
+    }
+
+    public void setCropName(String cropName) {
+        CropName = cropName;
+    }
+
+    public String getBotanicalName() {
+        return BotanicalName;
+    }
+
+    public void setBotanicalName(String botanicalName) {
+        BotanicalName = botanicalName;
+    }
+}
