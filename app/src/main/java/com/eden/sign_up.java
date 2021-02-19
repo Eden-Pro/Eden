ed85df3db69ba531edd84834cbb41132e70c8030
Vadefolarin <Adedayovictor457@gmail.com>
2021-02-19 09:08:40 +0200

    HomePage  added2

--- app/src/main/java/com/eden/sign_up.java
+++ app/src/main/java/com/eden/sign_up.java
@@ -3,12 +3,31 @@ package com.eden;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class sign_up extends AppCompatActivity {
+
+    public FirebaseAuth mAuth;
+    public EditText email;
+    public EditText password;
+    public EditText username;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth=FirebaseAuth.getInstance();
+
+    }
+
+    public void Register(View view) {
+
+
+
     }
 }
