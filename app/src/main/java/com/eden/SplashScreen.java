fcc062f4bb944e0b217610659a639d1b17cd9a86
Vadefolarin <Adedayovictor457@gmail.com>
2021-03-05 20:24:05 +0100

    HomePage  added3  HomepageContinue

--- app/src/main/java/com/eden/SplashScreen.java
+++ app/src/main/java/com/eden/SplashScreen.java
@@ -19,7 +19,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashScreen.this, AuthFragment.class);
+                Intent homeIntent = new Intent(SplashScreen.this, activity_home.class);
                 startActivity(homeIntent);
                 finish();
             }
